6e8ae26b14296bca54d9efbb4bb5fc85aa67e1ff
gyurika <gyorgykoszegi39@gmail.com>
2022-07-26 11:37:23 +0300

    create endpoint and input validator for finding all unique anagrams of a word in a sentence

--- routes/catalog.js
+++ routes/catalog.js
@@ -4,5 +4,6 @@ const anagramsFinderValidators = require('../validators/anagrams-finder.validato
 const anagramsFinderControllers = require('../controllers/anagrams-finder.controllers');
 
 router.post('/two-words-anagrams', anagramsFinderValidators.containsTwoWords, anagramsFinderControllers.twoWordsAnagram);
+router.post('/word-sentence-unique-anagrams', anagramsFinderValidators.containsWordSentence);
 
 module.exports = router;
